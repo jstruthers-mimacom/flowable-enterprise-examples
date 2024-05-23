796a48c17414479630f66055a394a7370831c06e
Jon Struthers <jon.struthers@mimacom.com>
2024-05-23 13:30:04 -0400

    Upgrade to Storybook 8.1.3

--- custom-form-component/.storybook/main.js
+++ custom-form-component/.storybook/main.js
@@ -1,4 +1,20 @@
-module.exports = {
-    stories: ['../src/**/*.stories.@(js|md|tsx)'],
-    addons: ['@storybook/addon-essentials', "@storybook/addon-links", "@storybook/addon-storysource", "@storybook/addon-cssresources"]
-}
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    '../src/**/*.stories.@(js|md|tsx)',
+  ],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-storysource",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+};
+export default config;
